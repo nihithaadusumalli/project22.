4b55af284b21bdb61b486426f3240feae5487f8c
nihithaadusumalli <76486929+nihithaadusumalli@users.noreply.github.com>
2021-01-24 08:45:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function preload()
 	starImg = loadImage("images/star.png");
 	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
 	bgImg = loadImage("images/starNight.png");
-	fairyVoice = loadSound("sound/JoyMusic.mp3");
+	fairyVoice = loadsound("sound/JoyMusic.mp3");
 
 }
 
@@ -49,5 +49,3 @@ function draw() {
 
 }
 
-function keyPressed() {
-}
